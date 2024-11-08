6318540cabc2b3b65ee20844bedccea377cdccca
456789123102 <aize.gong@gmail.com>
2024-11-08 19:16:05 +0700

    5K

---
+++ 5K_Rule30.js
@@ -0,0 +1,19 @@
+function rule30(list, n){
+    const zero = ['000','101','110','111'];
+while(n > 0){
+    list = [0,...list,0];
+    let newRow = [];
+    for (let i = 0; i < list.length; i++) {
+       let left = list[i-1] === undefined? 0 :  list[i-1];
+       let middle = list[i] === undefined? 0 : list[i];
+       let right = list[i+1] === undefined? 0 :  list[i+1];
+let cell = [left,middle,right].join("");
+zero.includes(cell) ? newRow.push(0) :  newRow.push(1);
+    }
+    list = newRow;
+    n--;
+}
+return list
+  }
+
+  console.log(rule30([1],2));
