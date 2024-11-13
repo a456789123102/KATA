b56adc3415242e40fa66ee1f0a64fe51857e3561
456789123102 <aize.gong@gmail.com>
2024-11-13 17:13:40 +0700

    6K

--- 6K_mod9.js
+++ 6K_mod9.js
@@ -1,8 +1,8 @@
-function nMod9(m,n){
-    let num = 0;
-    for(let i = m ; i <= n ; i++){
-num += i;
-    }
-return num%9
+function nMod9(m, n) {
+  let num = 0;
+  for (let i = m; i <= n; i++) {
+    num += i;
   }
-  console.log(nMod9(1,8))
+  return num % 9;
+}
+console.log(nMod9(1, 8));
