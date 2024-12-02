290318ff27f97d3aa052a77b9c8abbe5c146b4e4
456789123102 <aize.gong@gmail.com>
2024-12-02 14:33:56 +0700

    6k

---
+++ 6K_ATMmoneycounter.js
@@ -0,0 +1,32 @@
+const VALUES = {
+    "EUR": [5, 10, 20, 50, 100, 200, 500],
+    "USD": [1, 5, 10, 20, 50, 100, 500],
+    "UAH": [1, 5, 10, 20, 50, 100, 500],
+    "RUB": [5, 10, 50, 100, 500],
+    "CUP": [1, 5, 10, 20, 50],
+    "SOS": [1, 5, 10, 20, 50, 100]
+  };
+
+function atm(value) {
+    const currency = value.split('').filter(e => /[a-zA-Z]/.test(e)).join('').toUpperCase();
+    if(!VALUES[currency]) return `Sorry, have no ${currency}.`
+    let val =  parseFloat(value.split('').filter(e => !/[a-zA-Z]/.test(e)).join(''));
+
+    const sortedNote = VALUES[currency].slice().sort((a,b) => b-a);
+    if(val % sortedNote[sortedNote.length-1] !== 0) return `Can't do ${val} ${currency}. Value must be divisible by ${sortedNote[sortedNote.length-1]}!`
+
+   const result = [];
+
+   for(let i = 0 ; i < sortedNote.length ; i++){
+    if(sortedNote[i] <= val){
+        const amount = Math.floor(val/sortedNote[i]);
+        val -= sortedNote[i] * amount;
+        result.push(`${amount} * ${sortedNote[i]} ${currency}`)
+    }
+
+   }
+return result.join(', ')
+}
+console.log(atm("125rub"))
+'20 * 500 UAH, 2 * 100 UAH, 1 * 2 UAH'
+'20 * 500 UAH,2 * 100 UAH,1 * 2 UAH'
--- 7K_knapsackLight.js
+++ 7K_knapsackLight.js
@@ -29,16 +29,13 @@ function knapsackLight(value1, weight1, value2, weight2, maxW) {
         if (weights[i - 1] <= w) {
           //ตราบใดที่น้ำหนักยังไม่เกิน length ของ col
           dp[i][w] = Math.max(
-            dp[i - 1][w],
-            dp[i - 1][w - weights[i - 1]] + vals[i - 1]
-          ); //ค่าใช้จ่ายสูงสุด หรือ มูลค่าสูงสุด ที่สามารถเก็บได้ในกระเป๋า
+            dp[i - 1][w],dp[i - 1][w - weights[i - 1]] + vals[i - 1]); //ค่าใช้จ่ายสูงสุด หรือ มูลค่าสูงสุด ที่สามารถเก็บได้ในกระเป๋า
         } else {
           dp[i][w] = dp[i - 1][w];
         }
       }
     }
-    //return dp[n][maxW];
-    return dp
+    return dp[n][maxW];
   }
   return knapsack([value1, value2], [weight1, weight2], maxW);
 }
